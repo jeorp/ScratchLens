d9aefb69bf68731b110717666e6fee8e5c472488
jeorp <jeorpqxx@gmail.com>
2021-12-19 17:56:57 +0900

    test upd

--- test/LensSpec.hs
+++ test/LensSpec.hs
@@ -1,7 +1,9 @@
 module LensSpec where
 
 import Test.Hspec
-import Lens 
+import Lens
+
+import Control.Monad.State
 
 main :: IO ()
 main = hspec spec
@@ -33,6 +35,14 @@ p = Point 1 2
 foo :: Foo
 foo = Foo "ball" p 
 
+initPoint :: State Foo ()
+initPoint = point .= Point 0 0 
+
+getPointX :: State Foo Int
+getPointX = use (point . x)
+
+moveOneToY :: State Foo ()
+moveOneToY = (point . y) %= (+1)
 
 spec :: Spec
 spec = do
@@ -52,4 +62,11 @@ spec = do
       it "test make Lens4" $ do
         (name .~ "rect") foo `shouldBe` foo{_name="rect"}
 
+      it "test lens on context of state 1" $ do
+        runState initPoint  foo `shouldBe` ((), foo {_point = Point 0 0})
 
+      it "test lens on context of state 2" $ do
+        evalState getPointX  foo `shouldBe` 1
+
+      it "test lens on context of state 3" $ do
+        runState moveOneToY  foo `shouldBe` ((), foo {_point = Point 1 3})
